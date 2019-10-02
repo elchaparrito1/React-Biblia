1ba22655feff07937dcfb289022d95e7c32e5942
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2019-10-02 07:02:30 -0600

    new notes

--- Part-I.js
+++ Part-I.js
@@ -16,3 +16,16 @@
 /*Again, code gets transpiled into JS in the browser. This is why we have to use
 camelCasing for reserved JS words such as "class". When this element finally reaches
 the page and becomes HTML, it will be transpiled by Babel to "class".*/
+
+//We see this a lot in JSX:
+    <h1>{product.url}</h1>
+    //The braces ({}) around propertyvalues are a delimiter.
+        //These signal to JSX that what resides in-between the braces is a JS expression.
+    //The other delimiter is using quotes:
+        id='1'
+    //JSX attributes values must be delimited by either braces or quotes.
+
+//Dealing with "this" and props:
+    /*this is a special keyword in JS. The details of it are a bit nuanced, but for React
+    this is bound to the React component class. So when we use this.props inside of a
+    component, we're accessing the props property on that particular parent component. */
