bc355c6ca4fff985b5c76293b37e3c9b4b15c8bf
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2019-10-02 07:02:10 -0600

    New notes

---
+++ Part-I.js
@@ -0,0 +1,18 @@
+//Part I Notes:
+
+//JSX
+    /*Remember that even though JSX looks exactly like HTML, it is actually 
+    just compoiled into JS. During runtime React takes care of rendering actual
+    HTML in the browser for each component.*/
+
+    /*This essentially is the virtual DOM. JSX shows us what will eventually be
+    rendered in the DOM.*/
+
+//SyntaxError
+    /*This occurs when the JS engine encounters tokens or token order that doesn't
+    conform to the syntax of the language when parsing code. It indicates some code
+    is out of place or mistyped.*/
+
+/*Again, code gets transpiled into JS in the browser. This is why we have to use
+camelCasing for reserved JS words such as "class". When this element finally reaches
+the page and becomes HTML, it will be transpiled by Babel to "class".*/
